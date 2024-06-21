82957a51bd0e4359200dd0566b5a9f8095e38f61
Denis Nejelschi <nejelschi@gmail.com>
2024-06-21 20:14:45 +0200

    lesson8 part one

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,29 @@
+import './button.css';
+
+interface IbuttonProps{
+  isGetButton: boolean,
+  buttonText: string
+  onButtonClick?: () => void;
+}
+function Button({buttonText, isGetButton, onButtonClick}: IbuttonProps) {
+  const sendButton = 'Send data';
+  const getButton = 'Get data';
+  // от значения в этой переменной должен меняться цвет
+ 
+
+  // * запись тернарного оператора:
+  // { выражение ? если выражение верное : если выражение ложное }
+
+  return (
+    <button
+      onClick={onButtonClick}
+      className={`button ${isGetButton ? 'button-danger' : 'button-primary'}`}
+    >
+      {buttonText}
+    </button>
+  );
+}
+
+export default Button;
+
+// сделайте так, чтобы при значении true isGetButton кнопки были одного цвета, а при значении false другого
--- src/lessons/lesson08/Lesson08.tsx
+++ src/lessons/lesson08/Lesson08.tsx
@@ -1,10 +1,27 @@
-const Lesson08=()=>{
+import React from "react";
+import "./lesson08.css";
+import Button from "../../components/button/Button";
 
-    return(
-        <>
-        
-        </>
-    )
-
+const Lesson08 = () => {
+const consolePush = () => {
+    console.log("Push me");
 }
-export default Lesson08
+const consoleTouch = () => {
+    console.log("Touch me");
+}
+const consoleGet = () => {
+    console.log("Get satisfaction");
+}
+
+  return (
+    <div className="container">
+      <h1>Lesson 08 React on TS</h1>
+      <div className="buttons">
+        <Button buttonText="Push me" isGetButton={true} onButtonClick={consolePush} />
+        <Button buttonText="Touch me" isGetButton={false} onButtonClick={consoleTouch} />
+        <Button buttonText="Get satisfaction" isGetButton={true} onButtonClick={consoleGet} />
+      </div>
+    </div>
+  );
+};
+export default Lesson08;
