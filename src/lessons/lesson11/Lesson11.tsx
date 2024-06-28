298880f8281b7f3b762fc64b98fed090502d100e
Denis Nejelschi <nejelschi@gmail.com>
2024-06-28 23:28:05 +0200

    git logo and link added

--- src/lessons/lesson11/Lesson11.tsx
+++ src/lessons/lesson11/Lesson11.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import CatFacts from "../../components/catFacts/CatFacts";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./lesson11.css";
 
 export default function Lesson11() {
   return (
     <div>
-      <h1>Lesson11</h1>
+      <h1>Lesson11 
+      <a href="https://github.com/denisNejelschi/homeworksAF/tree/master/src/components/catCard" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+      
+        
+      </h1>
       <CatFacts />
       
     </div>
